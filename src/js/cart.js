405302118be09b69e186398cb88ef379b6f08387
Lee <karasactualemail@gmail.com>
2026-09-17 12:03:26 -0600

    build

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,6 +1,6 @@
 import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 
-// loadHeaderFooter();
+loadHeaderFooter();
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
