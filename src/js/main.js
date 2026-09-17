572b6990629408dc5dea4f9a2beb7787faecab9c
Lee <karasactualemail@gmail.com>
2026-09-17 12:01:45 -0600

    week03 team

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import "./newsletter.js";
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
 
