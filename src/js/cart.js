e3f25bb36e9291a2491ae161728619fcdebd33b9
Lee <karasactualemail@gmail.com>
2026-09-19 16:03:55 -0600

    cart subtotal

--- src/js/cart.js
+++ src/js/cart.js
@@ -6,25 +6,31 @@ loadHeaderFooter();
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  
+  const totalElement = document.querySelector(".cart-total");
+  const cartFooter = document.querySelector(".cart-footer");
+  let total = 0;
   const noItems = document.getElementById("no-items");
   if (cartItems.length != 0) {
     noItems.hidden = true;
+    cartFooter.hidden = false;
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     productList.innerHTML = htmlItems.join("");
+    cartItems.forEach(item => {
+      total += item.FinalPrice;
+    });
+    totalElement.textContent = `Subtotal: ${total.toFixed(2)}`;
   } else {
     noItems.hidden = false;
+    cartFooter.hidden = true;
+    productList.innerHTML = "";
   }
 }
-  
-
-
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
+      src="${item.Images.PrimaryMedium}"
       alt="${item.Name}"
     />
   </a>
@@ -34,7 +40,7 @@ function cartItemTemplate(item) {
   
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__remove" data-id="${item.ID}">X</p>
+  <p class="cart-card__remove" data-id="${item.Id}">X</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
 
@@ -50,9 +56,14 @@ productList.addEventListener("click", (event) => {
 
   let productID = event.target.dataset.id;
   let cartItems = getLocalStorage("so-cart") || [];
-  let index = cartItems.indexOf(productID);
+  let product = cartItems.find(item => item.Id === productID);
+  let index = cartItems.indexOf(product);
 
-  cartItems.splice(index, 1);
+  if (index !== -1) {
+    cartItems.splice(index, 1);
+  }
+
+  console.log(`removed ${productID}`)
 
   setLocalStorage("so-cart", cartItems);
 
